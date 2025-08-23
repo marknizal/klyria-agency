1b866f4f119b6abe9c394e4f2c0f9edf33e3e651
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-08-23 14:56:52 +0800

    add shared component style

---
+++ src/components/shared/index.js
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+import { color, breakpoint } from "../../styles";
+
+export const Main = styled.main`
+  display: block;
+`;
+
+export const Section = styled.section`
+  background: ${(props) => props.$bg || color.white};
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+
+  ${breakpoint.tablet} {
+    padding: 4rem 2rem;
+  }
+`;
+
+export const Paragraph = styled.p`
+  font-size: ${(props) => props.$size || "0.875rem"};
+  text-align: ${(props) => props.$align || "justify"};
+  line-height: 1.25rem;
+`;
