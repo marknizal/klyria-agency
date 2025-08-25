5dd35c6f78083f0b4c722171f00a18d74ee651f5
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-08-25 10:16:25 +0800

    feat: add loader component and use lazy on homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,38 +1,51 @@
-import { color, gradientBg } from "../styles";
+import { Suspense, lazy } from "react";
+
 import { Main, Section } from "../components/shared";
+import { color, gradientBg } from "../styles";
 
 import Navbar from "../components/common/navbar";
-import Hero from "../sections/hero";
-import Services from "../sections/services";
-import About from "../sections/about";
-import Feature from "../sections/feature";
-import Projects from "../sections/projects";
-import Cta from "../sections/cta";
 import Footer from "../components/common/footer";
+import Loader from "../components/common/loader";
+
+const Hero = lazy(() => import("../sections/hero"));
+const Services = lazy(() => import("../sections/services"));
+const About = lazy(() => import("../sections/about"));
+const Feature = lazy(() => import("../sections/feature"));
+const Projects = lazy(() => import("../sections/projects"));
+const Cta = lazy(() => import("../sections/cta"));
 
 const Homepage = () => {
   return (
     <Main>
-      <Navbar />
-      <Section $bg={gradientBg.hero}>
-        <Hero />
-      </Section>
-      <Section>
-        <Services />
-      </Section>
-      <Section $bg={gradientBg.about}>
-        <About />
-      </Section>
-      <Section>
-        <Feature />
-      </Section>
-      <Section $bg={gradientBg.projects}>
-        <Projects />
-      </Section>
-      <Section $bg={color.primary}>
-        <Cta />
-      </Section>
-      <Footer />
+      <Suspense fallback={<Loader />}>
+        <Navbar />
+
+        <Section $bg={gradientBg.hero}>
+          <Hero />
+        </Section>
+
+        <Section>
+          <Services />
+        </Section>
+
+        <Section $bg={gradientBg.about}>
+          <About />
+        </Section>
+
+        <Section>
+          <Feature />
+        </Section>
+
+        <Section $bg={gradientBg.projects}>
+          <Projects />
+        </Section>
+
+        <Section $bg={color.primary}>
+          <Cta />
+        </Section>
+
+        <Footer />
+      </Suspense>
     </Main>
   );
 };
